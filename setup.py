0af5f138d4224c6275fc32bbab37893c56ea2dd1
tobascofluf@yahoo.com <qzx7av@virginia.edugit config --global user.name Karthika Solaiqzx7av@virginia.edu>
2023-10-24 20:23:33 -0400

    add everything

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from boos import setup, find_packages
+from setuptools import setup
 
 setup(
     name='Boo',
     version='1.0.0',
+    descrip='a demo package about halloween',
     url='https://github.com/kat9019/Mod9Participation/tree/main',
-    author='Karthika Solai,
+    author='Karthika Solai',
     author_email='qzx7av@uva.edu',
     description='Prints Boo!',
-    packages=find_packages()    
+    packages=['boos']
 )
